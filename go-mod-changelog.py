ab76e9e54c82408581094eb172b522c405d4a22b
JM Faircloth <jmfaircloth@hashicorp.com>
2023-05-12 14:12:54 -0500

    bulk updates: add changelog blurb to PR

---
+++ go-mod-changelog.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# outputs a CHANGELOG blurb based on go.mod changes in the working dir
+
+import subprocess
+
+output = subprocess.check_output(
+    r"""git diff -- go.mod | cat | grep -ve indirect | grep -P '^[-+]\t'""", shell=True
+)
+
+minus = {}
+plus = {}
+
+for l in output.decode("utf8").strip().split("\n"):
+    if l.startswith("-"):
+        name, version = l[1:].strip().split(" ")
+        minus[name] = version
+    elif l.startswith("+"):
+        name, version = l[1:].strip().split(" ")
+        plus[name] = version
+
+keys = sorted(set(minus.keys()) | set(plus.keys()))
+print("IMPROVEMENTS:")
+print("* Updated dependencies:")
+for key in keys:
+    if key in minus and plus:
+        print(f"   * `{key}` {minus[key]} -> {plus[key]}")
+    elif key in minus:
+        print(f"   * `{key}` {minus[key]} removed")
+    else:
+        print(f"   * `{key}` {plus[key]} added")
